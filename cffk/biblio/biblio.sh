d28c4390cffdf7eebfa2685f3ae73c9704268837
Charles Karney <charles@karney.com>
2015-10-17 18:30:21 -0400

    Fix accents.

--- cffk/biblio/biblio.sh
+++ cffk/biblio/biblio.sh
@@ -2,6 +2,20 @@
 # Convert nbiblio.txt to an html page
 # git log --date=short $1 | head -3 | tail -1 | tr -s ' ' '	' |
 # cut -f2 | sed 's/$/./'
+cat <<EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <title>Charles Karney: Publications</title>
+    <meta name="description" content="Charles Karney: Publications">
+    <meta http-equiv="Content-Type"
+	  content="text/html; charset=ISO-8859-1">
+    <link rel="stylesheet" type="text/css" href="../default.css">
+    <style type="text/css"> li { margin-top: 5px; } </style>
+</head>
+
+EOF
 cat $1 |
 sed -e 's/\*/<li>/' \
     -e "s%'''\([0-9][---0-9A-Z]*\)'''%<b>\1</b>%g" \
@@ -13,4 +27,16 @@ sed -e 's/\*/<li>/' \
     -e 's%arxiv:\([^ ]*\)%<a href="http://arxiv.org/abs/\1">arxiv:\1</a>%g' \
     -e 's%inis:\([^ ]*\) \([^ ]*\)%<a href="https://inis.iaea.org/search/searchsinglerecord.aspx?recordsFor=SingleRecord\&RN=\1">\2</a>%g' \
     -e 's%patent:\([^ ]*\)%<a href="https://www.google.com/patents/\1">patent:\1</a>%g' \
-    -e 's%osti:\([^ ]*\) \([^ ]*\)%<a href="http://www.osti.gov/scitech/biblio/\1">\2</a>%g'
+    -e 's%osti:\([^ ]*\) \([^ ]*\)%<a href="http://www.osti.gov/scitech/biblio/\1">\2</a>%g' \
+    -e 's/É/\&Eacute;/g' \
+    -e 's/é/\&eacute;/g' \
+    -e 's/à/\&agrave;/g' \
+    -e 's/è/\&egrave;/g' \
+    -e 's/ù/\&ugrave;/g' \
+    -e 's/ç/\&ccedil;/g' \
+    -e 's/ä/\&auml;/g' \
+    -e 's/Ü/\&Uuml;/g' \
+    -e 's/ï/\&iuml;/g' \
+    -e 's/ö/\&ouml;/g' \
+    -e 's/ü/\&uuml;/g' \
+    -e 's/ß/\&szlig;/g'

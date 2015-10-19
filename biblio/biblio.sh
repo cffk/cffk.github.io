066f7f35d8bbd9b294c97d56d676eeb5a62f9fcb
Charles Karney <charles@karney.com>
2015-10-19 19:05:29 -0400

    Better link for inis entries.  Link for IPPJ report.

--- biblio/biblio.sh
+++ biblio/biblio.sh
@@ -16,17 +16,15 @@ cat <<EOF
 </head>
 
 EOF
-cat $1 |
+grep -v '^ *%' $1 |
 sed -e "s/@DATE@/$date/" \
     -e 's/\*/<li class=biblio>/' \
     -e "s%'''\([0-9][---0-9A-Z]*\)'''%<b>\1</b>%g" \
     -e "s% ''% <i>%g" -e "s%\([^ ]\)''%\1</i>%g" \
-    -e "s%---%\&mdash;%g" \
-    -e "s%\([^!]\)--\([^>]\)%\1\&ndash;\2%g" \
-    -e "s%\([^!]\)--\([^>]\)%\1\&ndash;\2%g" \
+    -e "s%---%\&mdash;%g" -e "s%--%\&ndash;%g" \
     -e 's%doi:\([^ ]*\)%<a href="https:dx.doi.org/\1">doi:\1</a>%g' \
     -e 's%ar[xX]iv:\([^ ]*\)%<a href="http://arxiv.org/abs/\1">arXiv:\1</a>%g' \
-    -e 's%inis:\([^ ]*\) \([^ ]*\)%<a href="https://inis.iaea.org/search/searchsinglerecord.aspx?recordsFor=SingleRecord\&RN=\1">\2</a>%g' \
+    -e 's%inis:\([^ ]*\) \([^ ]*\)%<a href="https://inis.iaea.org/search/search.aspx?orig_q=RN:\1">\2</a>%g' \
     -e 's%patent:\([^ ]*\)%<a href="https://www.google.com/patents/\1">patent:\1</a>%g' \
     -e 's%osti:\([^ ]*\) \([^ ]*\)%<a href="http://www.osti.gov/scitech/biblio/\1">\2</a>%g' \
     -e 's/É/\&Eacute;/g' \
